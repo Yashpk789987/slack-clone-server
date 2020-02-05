504c96a21e9c53248d5aa0991bf2864b86666b69
yash <yashpk789987@gmail.com>
2020-02-06 01:50:14 +0530

    added child resolvers protected create team if user not authenticated

--- resolvers/team.js
+++ resolvers/team.js
@@ -1,14 +1,23 @@
 import formatErrors from '../formatErrors';
+import requiresAuth from '../permissions';
+
 export default {
   Mutation: {
-    createTeam: async (parent, args, context) => {
-      try {
-        await context.models.Team.create({ ...args, owner: context.user.id });
-        return { ok: true };
-      } catch (error) {
-        console.log(error);
-        return { ok: false, errors: formatErrors(error) };
+    createTeam: requiresAuth.createResolver(
+      async (parent, args, { models, user }) => {
+        try {
+          await models.Team.create({ ...args, owner: user.id });
+          return {
+            ok: true
+          };
+        } catch (err) {
+          console.log(err);
+          return {
+            ok: false,
+            errors: formatErrors(err)
+          };
+        }
       }
-    }
+    )
   }
 };

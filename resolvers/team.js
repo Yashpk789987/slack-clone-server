8cd78daf6d0cc9c3297694ead96dec71029b30ae
yash <yashpk789987@gmail.com>
2020-02-20 02:08:13 +0530

    commited

--- resolvers/team.js
+++ resolvers/team.js
@@ -13,6 +13,61 @@ export default {
     )
   },
   Mutation: {
+    addTeamMember: requiresAuth.createResolver(
+      async (parent, { email, teamId }, { models, user }) => {
+        try {
+          const teamPromise = models.Team.findOne(
+            {
+              where: { id: teamId }
+            },
+            { raw: true }
+          );
+
+          const usertoAddPromise = models.User.findOne(
+            { where: { email } },
+            { raw: true }
+          );
+
+          const [team, usertoAdd] = await Promise.all([
+            teamPromise,
+            usertoAddPromise
+          ]);
+
+          if (team.owner !== user.id) {
+            return {
+              ok: false,
+              errors: [
+                { path: 'email', message: 'You cannot add members to the team' }
+              ]
+            };
+          }
+
+          if (!usertoAdd) {
+            return {
+              ok: false,
+              errors: [
+                {
+                  path: 'email',
+                  message: 'Could not find user with this email'
+                }
+              ]
+            };
+          }
+
+          await models.Member.create({ userId: usertoAdd.id, teamId });
+
+          return {
+            ok: true
+          };
+        } catch (err) {
+          console.log(err);
+          return {
+            ok: false,
+            errors: formatErrors(err)
+          };
+        }
+      }
+    ),
     createTeam: requiresAuth.createResolver(
       async (parent, args, { models, user }) => {
         try {

f49d236baee528ac9f3ad4fd2895c5dba12c79f1
yash <yashpk789987@gmail.com>
2020-03-08 15:36:10 +0530

    Updating Graphql Cache

--- resolvers/user.js
+++ resolvers/user.js
@@ -21,6 +21,8 @@ export default {
     me: requiresAuth.createResolver((parent, args, { models, user }) =>
       models.User.findOne({ where: { id: user.id } })
     ),
+    getUser: (parent, { userId }, { models }) =>
+      models.User.findOne({ where: { id: userId } }),
     allUsers: (parent, args, { models }) => models.User.findAll()
   },
   Mutation: {

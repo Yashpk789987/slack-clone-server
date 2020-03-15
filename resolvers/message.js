0de79aaf46a568415638a926db9069b4195f6f52
yash <yashpk789987@gmail.com>
2020-03-16 03:35:23 +0530

    Offset Pagination Done

--- resolvers/message.js
+++ resolvers/message.js
@@ -29,7 +29,7 @@ export default {
   },
   Query: {
     messages: requiresAuth.createResolver(
-      async (parent, { channelId }, { models, user }) => {
+      async (parent, { channelId, offset }, { models, user }) => {
         const channel = await models.Channel.findOne({
           raw: true,
           where: { id: channelId }
@@ -46,7 +46,12 @@ export default {
         }
 
         return models.Message.findAll(
-          { order: [['created_at', 'ASC']], where: { channelId } },
+          {
+            order: [['created_at', 'ASC']],
+            where: { channelId },
+            limit: 35,
+            offset
+          },
           { raw: true }
         );
       }
